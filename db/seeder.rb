4d80492fdf0672866ec8ca758eb5f0a6ddbc3ad8
OtterLover07 <melker@willforss.se>
2026-01-16 09:45:46 +0100

    Påbörjat inloggningssystem
    Registrering fungerar

--- db/seeder.rb
+++ db/seeder.rb
@@ -14,42 +14,48 @@ def seed!(db)
   puts "✅ Done seeding the database!"
 end
 
-def drop_tables(db)
-  db.execute('DROP TABLE IF EXISTS todos')
-  db.execute('DROP TABLE IF EXISTS categories')
-  db.execute('DROP TABLE IF EXISTS todo_cat_rel')
-end
+# def drop_tables(db)
+#   db.execute('DROP TABLE IF EXISTS todos')
+#   db.execute('DROP TABLE IF EXISTS categories')
+#   db.execute('DROP TABLE IF EXISTS todo_cat_rel')
+#   db.execute('DROP TABLE IF EXISTS users') 
+# end
 
 def create_tables(db)
-  db.execute('CREATE TABLE todos (
+  db.execute('CREATE TABLE IF NOT EXISTS todos (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
               notes TEXT,
-              done BOOLEAN DEFAULT 0)')
-  db.execute('CREATE TABLE categories (
+              done BOOLEAN DEFAULT 0,
+              belongs_to INT)')
+  db.execute('CREATE TABLE IF NOT EXISTS categories (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name TEXT NOT NULL)')
-  db.execute('CREATE TABLE todo_cat_rel (
+              name TINYTEXT NOT NULL)')
+  db.execute('CREATE TABLE IF NOT EXISTS todo_cat_rel (
               todo_id INT NOT NULL,
               cat_id INT NOT NULL,
               UNIQUE (todo_id, cat_id) ON CONFLICT IGNORE)')
-  # db.execute('')
+   db.execute('CREATE TABLE IF NOT EXISTS users (
+              user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TINYTEXT UNIQUE NOT NULL,
+              pwd_digest TEXT NOT NULL,
+              admin BOOLEAN DEFAULT 0)')
       
 end
 
-def populate_tables(db)
-  db.execute('INSERT INTO todos (name, notes) VALUES ("Add toggle complete", "Do this last")')
-  db.execute('INSERT INTO todos (name, notes) VALUES ("Add Edit functionality test", "if this is added stuff worked test")')
-  db.execute('INSERT INTO todos (name, notes) VALUES ("Add categories", "Searchable categories")')
+# def populate_tables(db)
+#   db.execute('INSERT INTO todos (name, notes) VALUES ("Add toggle complete", "Do this last")')
+#   db.execute('INSERT INTO todos (name, notes) VALUES ("Add Edit functionality test", "if this is added stuff worked test")')
+#   db.execute('INSERT INTO todos (name, notes) VALUES ("Add categories", "Searchable categories")')
 
-  db.execute('INSERT INTO categories (name) VALUES ("admin")')
-  db.execute('INSERT INTO categories (name) VALUES ("development")')
-  db.execute('INSERT INTO categories (name) VALUES ("purchases")')
+#   db.execute('INSERT INTO categories (name) VALUES ("admin")')
+#   db.execute('INSERT INTO categories (name) VALUES ("development")')
+#   db.execute('INSERT INTO categories (name) VALUES ("purchases")')
 
-  db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (1, 2)')
-  db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (2, 2)')
-  db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (3, 2)')
-  db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (1, 1)')
-end
+#   db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (1, 2)')
+#   db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (2, 2)')
+#   db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (3, 2)')
+#   db.execute('INSERT INTO todo_cat_rel (todo_id, cat_id) VALUES (1, 1)')
+# end
 
 seed!(db)
--- login.rb
+++ login.rb
@@ -1,6 +1,36 @@
+require 'bcrypt'
+
+get('/register') do
+    slim(:register)
+end
+
+post('/register') do
+    username = params[:username]
+    password, confirm_password = params[:password], params[:pwd_confirm]
+    admin = (params[:admin] == "true" ? "1" : "0")
+
+    username_check = db.execute('SELECT user_id FROM users WHERE username=?', username.downcase)
+    if username_check.empty?
+        if password == confirm_password
+            pwd_digest = BCrypt::Password.create(password)
+            db.execute('INSERT INTO users (username, pwd_digest, admin) VALUES (?, ?, ?)', [username.downcase, pwd_digest, admin])
+            redirect('/')
+        else
+            session[:pwd_fail] = "Passwords must match."
+            redirect('/register')
+        end
+    else
+        session[:username_fail] = "Username already taken"
+        redirect('/register')
+    end
+end
+
+
+###################### FIXA LOGIN ####################
 post('/login') do
-    pass = params[:pass]
-    if pass == "AdminPassword1234"
+    pwd = params[:pwd]
+    if pwd == "AdminPassword1234"
+        session[:user_id] = true #ÄNDRA SEN
         session[:admin] = true
         flash[:login] = "Info: Login successful"
     else
@@ -10,7 +40,6 @@ post('/login') do
 end
 
 post('/logout') do
-    session[:admin] = false
-    flash[:login] = "Info: Logged out"
+    session.clear
     redirect('/')
 end
